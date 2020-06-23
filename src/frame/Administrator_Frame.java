0c97b4bf6a606acf86c5ba8783e5084258f1f4b6
pancrasw <qwert123456001@163.com>
2020-06-23 16:58:50 +0800

    借阅管理界面控件布置

--- src/frame/Administrator_Frame.java
+++ src/frame/Administrator_Frame.java
@@ -3,22 +3,38 @@ package frame;
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
 import javax.swing.Box;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
+import javax.swing.table.DefaultTableModel;
 
 import service.BookManage_Service;
+import service.BookSearch_Service;
+import service.ReaderManage_Service;
+import model.Book;
+import model.Borrow;
+import model.Reader;
 
 public class Administrator_Frame extends JFrame{
-	String m_ID;
+	private String m_ID;
+	private static BookSearch_Service bookSearch_Service=BookSearch_Service.getInstance();
+	private static ReaderManage_Service readerManage_Service=ReaderManage_Service.getInstance();
 	public Administrator_Frame(String ID) {
+		m_ID=ID;
 		//界面初始化
 		setDefaultLookAndFeelDecorated(true);
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
@@ -98,28 +114,41 @@ public class Administrator_Frame extends JFrame{
 		blankPanel.setBackground(Color.white);
 		
 		//图书管理面板
+		JPanel bookContentPanel=new JPanel();
+		bookManagePanel.add(bookContentPanel);
 		JLabel bookidLabel=new JLabel("书ID号/book ID");
-		bookManagePanel.add(bookidLabel);
+		bookContentPanel.add(bookidLabel);
 		JTextField bookidTextField=new JTextField(10);
-		bookManagePanel.add(bookidTextField);
+		bookContentPanel.add(bookidTextField);
 		JLabel bookNameLabel=new JLabel("书名/book name");
-		bookManagePanel.add(bookNameLabel);
+		bookContentPanel.add(bookNameLabel);
 		JTextField bookNameTextField=new JTextField();
-		bookManagePanel.add(bookNameTextField);
+		bookContentPanel.add(bookNameTextField);
 		JLabel authorLabel=new JLabel("作者/author");
-		bookManagePanel.add(authorLabel);
+		bookContentPanel.add(authorLabel);
 		JTextField authorTextField=new JTextField();
-		bookManagePanel.add(authorTextField);
+		bookContentPanel.add(authorTextField);
 		JLabel publisherLabel=new JLabel("出版社/publisher");
-		bookManagePanel.add(publisherLabel);
+		bookContentPanel.add(publisherLabel);
 		JTextField publisherTextField=new JTextField();
-		bookManagePanel.add(publisherTextField);
-		JButton searchButton=new JButton("搜索/search");
-		bookManagePanel.add(searchButton);
+		bookContentPanel.add(publisherTextField);
+		JLabel typeLabel=new JLabel("分类/type");
+		bookContentPanel.add(typeLabel);
+		String[] booktypestrings= {"所有","文学","哲学","小说","科学","教材","军事","经济","历史"};
+		JComboBox<String> bookTypeComboBox=new JComboBox<String>(booktypestrings);
+		bookContentPanel.add(bookTypeComboBox);
+		JButton bookSearchButton=new JButton("搜索/search");
+		bookContentPanel.add(bookSearchButton);
+		JTable bookSearchResulTable=new JTable() {
+			public boolean isCellEditable(int rowIndex, int ColIndex){
+				return false;
+			}
+		};
+		JScrollPane bookTableScrollPane=new JScrollPane(bookSearchResulTable);
+		bookManagePanel.add(bookTableScrollPane);
 		bookidTextField.getDocument().addDocumentListener(new DocumentListener() {
 			@Override
 			public void removeUpdate(DocumentEvent e) {
-				// TODO Auto-generated method stub
 				if(bookidTextField.getText().length()==0) {
 					bookNameTextField.setEditable(true);
 					authorTextField.setEditable(true);
@@ -128,20 +157,164 @@ public class Administrator_Frame extends JFrame{
 			}
 			@Override
 			public void insertUpdate(DocumentEvent e) {
-				// TODO Auto-generated method stub
 				bookNameTextField.setEditable(false);
 				authorTextField.setEditable(false);
 				publisherTextField.setEditable(false);
 			}
 			@Override
 			public void changedUpdate(DocumentEvent e) {
-				// TODO Auto-generated method stub	
+				//do nothing
 			}
 		});
+		Object[] bookAttributeObjects= {"书号","书名","价格","类型","作者","出版社","库存数量","简介"};
+		DefaultTableModel bookSearchResultTableModel =new DefaultTableModel(bookAttributeObjects, 0);
+		bookSearchResulTable.setModel(bookSearchResultTableModel);
+		bookSearchResulTable.getSelectionModel().setSelectionMode(ListSelectionModel.SINGLE_SELECTION);//
+		bookSearchButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				if(bookidTextField.getText().length()!=0) {
+					List<Book> searchresult =bookSearch_Service.searchByBookID(bookidTextField.getText());
+					String[]arr=new String[8];
+					Book book=searchresult.get(0);
+					arr[0]=book.getIdBook();
+					arr[1]=book.getNameBook();
+					arr[2]=String.valueOf(book.getPrice());
+					arr[3]=book.getType();
+					arr[4]=book.getAuthor();
+					arr[5]=book.getPublisher();
+					arr[6]=String.valueOf(book.getAmount());
+					arr[7]=book.getIntro();
+					bookSearchResultTableModel.addRow(arr);
+				}
+				else {
+					String typestring=(String)bookTypeComboBox.getSelectedItem();
+					if(typestring=="所有")typestring="";
+					List<Book> searchresult = bookSearch_Service.searchByBookInfo("", bookNameTextField.getText(), typestring, 
+						authorTextField.getText(), publisherTextField.getText());
+					for(Book book:searchresult) {
+						String[]arr=new String[8];
+						arr[0]=book.getIdBook();
+						arr[1]=book.getNameBook();
+						arr[2]=String.valueOf(book.getPrice());
+						arr[3]=book.getType();
+						arr[4]=book.getAuthor();
+						arr[5]=book.getPublisher();
+						arr[6]=String.valueOf(book.getAmount());
+						arr[7]=book.getIntro();
+						bookSearchResultTableModel.addRow(arr);
+					}
+				}
+			}
+		});
+		bookContentPanel.setLayout(new GridLayout(2,6));
+		bookSearchResulTable.setPreferredScrollableViewportSize(new Dimension(550,400));
 		
 		//借阅管理面板
-		
-		
+		JPanel readerSearchGridPanel=new JPanel();
+		borrowManagePanel.add(readerSearchGridPanel);
+		JTextField readerSearchReferenceTextField=new JTextField();
+		readerSearchGridPanel.add(readerSearchReferenceTextField);
+		String[] readerSearchModeStrings= {"通过读者ID搜索","通过读者姓名搜索"};
+		JComboBox<String> readerSearchModeComboBox=new JComboBox<String>(readerSearchModeStrings);
+		readerSearchGridPanel.add(readerSearchModeComboBox); 
+		JButton readerSearchButton=new JButton("搜索读者");
+		readerSearchGridPanel.add(readerSearchButton);
+		JTable borrowSearchResulTable=new JTable() {
+			public boolean isCellEditable(int rowIndex, int ColIndex){
+				return false;
+			}
+		};
+		JScrollPane borrowSearchReasultTableScrollPane=new JScrollPane(borrowSearchResulTable);
+		borrowManagePanel.add(borrowSearchReasultTableScrollPane);
+		Object[] borrowAttributeObjects= {"书号","书名","类型","作者","出版社","借阅日期","归还日期","是否过期未还"};
+		DefaultTableModel borrowSearchResultTableModel =new DefaultTableModel(borrowAttributeObjects, 0);
+		borrowSearchResulTable.setModel(borrowSearchResultTableModel);
+		borrowSearchResulTable.getSelectionModel().setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		readerSearchButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				int readerSearchMode=readerSearchModeComboBox.getSelectedIndex();
+				if(readerSearchMode==0) {
+					List<Borrow> borrows= bookSearch_Service.searchBorrowInfo(readerSearchReferenceTextField.getText());
+					for(Borrow borrow:borrows) {
+						List<Book> searchresult =bookSearch_Service.searchByBookID(borrow.getIdBook());
+						String[]arr=new String[8];
+						Book book=searchresult.get(0);
+						arr[0]=book.getIdBook();
+						arr[1]=book.getNameBook();
+						arr[2]=book.getType();
+						arr[3]=book.getAuthor();
+						arr[4]=book.getPublisher();
+						arr[5]=borrow.getLendDate().toString();
+						arr[6]=borrow.getDueDate().toString();
+						arr[7]=borrow.getOvertime();
+						borrowSearchResultTableModel.addRow(arr);
+					}
+				}
+				else {
+					List<Reader>readers= readerManage_Service.SearchReaderByName(readerSearchReferenceTextField.getText());
+					JFrame selectReaderFrame=new JFrame();
+					selectReaderFrame.setTitle("请选择所要查看的读者并点击确认");
+					JPanel selectReaderPanel=new JPanel();
+					selectReaderFrame.add(selectReaderPanel);
+					JTable readerSearchResulTable=new JTable() {
+						public boolean isCellEditable(int rowIndex, int ColIndex){
+							return false;
+						}
+					};
+					JScrollPane readerSearchResulTableScrollPane=new JScrollPane(readerSearchResulTable);
+					selectReaderPanel.add(readerSearchResulTableScrollPane);
+					Object[] readerAttributeObjects= {"读者号","读者名","读者类型","读者性别"};
+					DefaultTableModel readerSearchResultTableModel =new DefaultTableModel(readerAttributeObjects, 0);
+					readerSearchResulTable.setModel(readerSearchResultTableModel);
+					readerSearchResulTable.getSelectionModel().setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+					for(Reader reader:readers) {
+						String[]arr=new String[4];
+						arr[0]=reader.getIdReader();
+						arr[1]=reader.getNameReader();
+						arr[2]=reader.getType();
+						arr[3]=reader.getSex();
+						readerSearchResultTableModel.addRow(arr);
+					}
+					JButton selectReaderButton=new JButton("确认");
+					selectReaderPanel.add(selectReaderButton);
+					selectReaderButton.addActionListener(new ActionListener() {
+						@Override
+						public void actionPerformed(ActionEvent e) {
+							// TODO Auto-generated method stub
+							int selectrow=readerSearchResulTable.getSelectedRow();
+							String rIDString=(String)readerSearchResulTable.getValueAt(selectrow, 0);
+							List<Borrow> borrows= bookSearch_Service.searchBorrowInfo(rIDString);
+							for(Borrow borrow:borrows) {
+								List<Book> searchresult =bookSearch_Service.searchByBookID(borrow.getIdBook());
+								String[]arr=new String[8];
+								Book book=searchresult.get(0);
+								arr[0]=book.getIdBook();
+								arr[1]=book.getNameBook();
+								arr[2]=book.getType();
+								arr[3]=book.getAuthor();
+								arr[4]=book.getPublisher();
+								arr[5]=borrow.getLendDate().toString();
+								arr[6]=borrow.getDueDate().toString();
+								arr[7]=borrow.getOvertime();
+								borrowSearchResultTableModel.addRow(arr);
+							}
+							selectReaderFrame.dispose();
+						}
+					});
+					readerSearchResulTable.setPreferredScrollableViewportSize(new Dimension(350,200));
+					selectReaderFrame.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+					selectReaderFrame.pack();
+					selectReaderFrame.setVisible(true);
+					selectReaderFrame.setBounds(200, 200, 370,300);
+				}
+			}
+		});
+		readerSearchGridPanel.setLayout(new GridLayout(1,3));
+		borrowSearchResulTable.setPreferredScrollableViewportSize(new Dimension(550,400));
 		//读者管理面板
 		//添加图书面板
 		
